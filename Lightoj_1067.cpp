427a57921f5b5ea5e59b0b67cce7f2b9e951170a
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2020-05-31 01:18:13 +0600

    Combination,BigMod,InverseMod

---
+++ Lightoj_1067.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define lightoj_test_case_loop(test) for(int cs=1;cs<=test;cs++)
+#define lightoj_output cout<<"Case "<<cs<<": "
+#define pb push_back
+#define mp make_pair
+#define white 0
+#define grey 1
+#define black 2
+#define ll long long
+int fx[]={+1,-1,0,0};
+int fy[]={0,0,+1,-1};
+int turnOn(int x,int pos) {
+    return x | (1<<pos-1);
+}
+bool isOn(int N,int pos) {
+    return (bool)(N & (1<<pos-1));
+}
+///lcs using dp
+/*
+int lcs_dp[31][31]={0};
+int longest_common_sub(int i,int j,string &a,string &b)
+{
+    if(i==a.length()||j==b.length())
+        return 0;
+    if(lcs_dp[i][j]!=-1)
+        return lcs_dp[i][j];
+    int ans=0;
+    if(a[i]==b[j])
+        ans=1+longest_common_sub(i+1,j+1,a,b);
+    else
+        ans=max(longest_common_sub(i+1,j,a,b),longest_common_sub(i,j+1,a,b));
+    return lcs_dp[i][j]=ans;
+
+}
+*/
+///lightoj 1067
+#define modvalue 1000003
+#define mxsize 1000001
+ll fac[mxsize];
+void CalculateFactorial()
+{
+    fac[0]=1;
+    for(int i=1;i<=mxsize;i++)
+        fac[i]=((fac[i-1])%modvalue*(i%modvalue))%modvalue;
+}
+//int cnt=0;
+ll BigMod(ll base,ll power,ll mod)
+{
+    //cnt++;
+    //cout<<power
+    if(power==0)
+        return 1;
+    if(power%2==1)
+        return (base%mod)*(BigMod(base,power-1,mod)%mod)%mod;
+    else
+    {
+        ll res = (BigMod(base,power/2,mod)%mod);
+        return (res*res)%mod;
+
+    }
+}
+int main()
+{
+    CalculateFactorial();
+    int test_case;
+    cin>>test_case;
+    lightoj_test_case_loop(test_case)
+    {
+        ll n,r;
+        cin>>n>>r;
+        int res1=(BigMod(fac[r],modvalue-2,modvalue)%modvalue*BigMod(fac[n-r],modvalue-2,modvalue)%modvalue)%modvalue;
+        int res=(fac[n]%modvalue*res1%modvalue)%modvalue;
+        lightoj_output<<res<<endl;
+    }
+}
