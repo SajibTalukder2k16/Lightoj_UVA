990cb075b9530d15739b0aa0b4838e2164cd9fe3
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2020-06-03 18:59:14 +0600

    Lightoj, Game Theory Algorithm

---
+++ Lightoj_1192.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define lightoj_test_case_loop(test) for(int cs=1;cs<=test;cs++)
+#define lightoj_output cout<<"Case "<<cs<<": "
+#define pb push_back
+#define mp make_pair
+#define white 0
+#define grey 1
+#define black 2
+#define ll long long
+int fx[]={+1,-1,0,0};
+int fy[]={0,0,+1,-1};
+int turnOn(int x,int pos) {
+    return x | (1<<pos-1);
+}
+bool isOn(int N,int pos) {
+    return (bool)(N & (1<<pos-1));
+}
+///lcs using dp
+/*
+int lcs_dp[31][31]={0};
+int longest_common_sub(int i,int j,string &a,string &b)
+{
+    if(i==a.length()||j==b.length())
+        return 0;
+    if(lcs_dp[i][j]!=-1)
+        return lcs_dp[i][j];
+    int ans=0;
+    if(a[i]==b[j])
+        ans=1+longest_common_sub(i+1,j+1,a,b);
+    else
+        ans=max(longest_common_sub(i+1,j,a,b),longest_common_sub(i,j+1,a,b));
+    return lcs_dp[i][j]=ans;
+
+}
+*/
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    lightoj_test_case_loop(test_case)
+    {
+        int n;
+        cin>>n;
+        n*=2;
+        int ara[n+1];
+        int ans=0;
+        int value;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>ara[i];
+            if(i%2==0)
+            {
+                value=ara[i]-ara[i-1]-1;
+                ans^=value;
+            }
+        }
+        if(ans==0)
+            lightoj_output<<"Bob"<<endl;
+        else
+            lightoj_output<<"Alice"<<endl;
+
+    }
+}
