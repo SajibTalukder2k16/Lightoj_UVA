7d59d52f9442b5d8f29e9e4f22e6ba7db661df6e
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2021-05-19 23:35:18 +0600

    Update lightoj_1007.cpp

--- lightoj_1007.cpp
+++ lightoj_1007.cpp
@@ -1,36 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mx 5000001
+#define ll unsigned long long
+#define M 5000002
+int marked[M/64 + 2];
+ll euler[M+2];
+
+#define on(x) (marked[x/64] & (1<<((x%64)/2)))
+#define mark(x)  marked[x/64] |= (1<<((x%64)/2))
+
+void sieve(int n) {
+  for (ll i = 3; i<=n; i += 2) {
+    if (!on(i)) {
+      for (ll j = i ; j <= n; j += i ) {
+        if(j%2==1)
+            mark(j);
+        euler[j]=(euler[j]/i)*(i-1);
+      }
+    }
+  }
+}
+
+bool isPrime(int num) {
+  return num > 1 && (num == 2 || ((num & 1) && !on(num)));
+}
 int main()
 {
     //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
-    vector<unsigned long long>euler(mx);
-    euler[1]=1;
-    for(int i=2;i<mx;i++)
+    for(int i=1;i<M;i++)
     {
-        euler[i]=i-1;
+        euler[i]=i;
+        if(i%2==0)
+            euler[i]/=2;
     }
-    for(int i=2;i<mx;i++)
-    {
-        for(int j=i+i;j<mx;j+=i)
-        {
-            euler[j]=euler[j]-euler[i];
-        }
-    }
+    sieve(M);
     euler[0]=0;
-    for(int i=1;i<mx;i++)
+    for(int i=2;i<M;i++)
     {
-        euler[i] = euler[i]*euler[i];
-        euler[i] = euler[i]+euler[i-1];
+        euler[i]=euler[i]*euler[i];
+        euler[i]+=euler[i-1];
     }
     int test,cs;
-    cin>>test;
+    scanf("%d",&test);
     int a,b;
     for(cs=1;cs<=test;cs++)
     {
-        cin>>a>>b;
+        scanf("%d%d",&a,&b);
         a--;
-        cout<<"Case "<<cs<<": "<<euler[b]-euler[a]<<endl;
+        //cout<<euler[b]<<" "<<euler[a]<<endl;
+        //cout<<"Case "<<cs<<": "<<euler[b]-euler[a]<<endl;
+        printf("Case %d: %llu\n",cs,euler[b]-euler[a]);
     }
+    return 0;
 }
